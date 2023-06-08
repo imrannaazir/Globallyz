8b801ead00f57d024e3c3df6a1ab316bd410d2b0
emon <imrannaaziremon@gmail.com>
2023-06-08 15:42:58 +0600

    update icon color

--- src/components/Pricing/ServicePricingRow.jsx
+++ src/components/Pricing/ServicePricingRow.jsx
@@ -7,17 +7,29 @@ export default function ServicePricingRow({ service }) {
       <td>{service?.service}</td>
       <td>
         <div className="flex items-center justify-center">
-          {!service?.basic ? <BsXCircle /> : <AiOutlineCheckCircle />}
+          {!service?.basic ? (
+            <BsXCircle className="text-red-400" />
+          ) : (
+            <AiOutlineCheckCircle className="text-green-400" />
+          )}
         </div>
       </td>
       <td>
         <div className="flex items-center justify-center">
-          {!service?.premium ? <BsXCircle /> : <AiOutlineCheckCircle />}
+          {!service?.premium ? (
+            <BsXCircle className="text-red-400" />
+          ) : (
+            <AiOutlineCheckCircle className="text-green-400" />
+          )}
         </div>
       </td>
       <td>
         <div className="flex items-center justify-center">
-          {!service?.elite ? <BsXCircle /> : <AiOutlineCheckCircle />}
+          {!service?.elite ? (
+            <BsXCircle className="text-red-400" />
+          ) : (
+            <AiOutlineCheckCircle className="text-green-400" />
+          )}
         </div>
       </td>
     </tr>
